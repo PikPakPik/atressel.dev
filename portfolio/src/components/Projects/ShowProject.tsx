fc94cb2300a72d2ad3c99ba3bf3352ecd087b637
Alexandre Tressel <alexandre.tressel@epitech.eu>
2023-09-29 19:41:52 +0200

    style: :iphone: Change the style presentation for project detail

--- portfolio/src/components/Projects/ShowProject.tsx
+++ portfolio/src/components/Projects/ShowProject.tsx
@@ -1,47 +1,67 @@
 import { useParams } from "react-router-dom";
-import { Project } from "../../types/project"
+import { Project } from "../../types/project";
 import { useEffect, useState } from "react";
 import { projects } from "../../data/projects";
 
 const ShowProject: React.FC = () => {
-    const { projectId } = useParams<{ projectId: any }>();
-    const [project, setProject] = useState<Project | null>(null);
+  let { projectId } = useParams<{ projectId: string | any }>();
+  const [project, setProject] = useState<Project | null>(null);
 
-    useEffect(() => {
-        const foundProject = projects.find((p) => p.id === projectId);
+  useEffect(() => {
+    projectId = parseInt(projectId);
+    const foundProject = projects.find((project) => project.id === projectId);
 
-        if (foundProject) {
-            setProject(foundProject);
-        } else {
-            setProject(null);
-        }
-    }, [projectId, projects]);
+    if (foundProject) {
+      setProject(foundProject);
+    } else {
+      setProject(null);
+    }
+  }, [projectId, projects]);
 
-
-    return (
-        <>
-            <div id="projects" className="container mx-auto lg:px-32  my-[10rem] md:my-25">
-                <div className="flex flex-row items-center justify-center md:justify-start">
-                    <h2 className="text-3xl text-white"><span className="text-[#C778DD]">#</span>projets</h2>
-                    <div className="w-[50%] h-[2px] bg-[#C778DD] ml-4 hidden md:block"></div>
+  return (
+    <>
+      <div id="projects" className="min-h-screen p-16 mt-12">
+        <div className="container mx-auto px-4 md:px-8 lg:px-16">
+          <div className="text-center mb-8">
+            <h2 className="text-4xl font-bold text-white">
+              {project ? project.name : "Project Not Found"}
+            </h2>
+            <div className="w-16 h-1 bg-[#C778DD] mx-auto mt-4"></div>
+          </div>
+          {project && (
+            <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8">
+              <div className="md:w-1/2">
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  className="object-cover rounded-lg w-full"
+                />
+              </div>
+              <div className="md:w-1/2 mt-6 md:mt-0">
+                <div className="text-white">
+                  <h3 className="text-2xl font-semibold">Description</h3>
+                  <p className="mt-2">{project.description}</p>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-10 mt-10 justify-items-center sm:px-0 px-10">
-                    {project && (
-                        <div className="flex flex-col items-center justify-center">
-                            <h3 className="text-2xl text-white font-bold">{project.name}</h3>
-                            <div className="w-[50%] h-[2px] bg-[#C778DD]"></div>
-                            <p className="text-white text-center mt-4">{project.description}</p>
-                            <div className="flex flex-row items-center justify-center gap-4 mt-4">
-                                {project.technologies.map((technology, index) => (
-                                    <span key={index} className="text-white text-sm">{technology.name}</span>
-                                ))}
-                            </div>
-                        </div>
-                    )}
+                <div className="mt-6">
+                  <h3 className="text-2xl font-semibold text-white">Technologies</h3>
+                  <div className="mt-2 space-x-4">
+                    {project.technologies.map((technology, index) => (
+                      <span
+                        key={index}
+                        className="text-[#C778DD] border border-[#C778DD] px-2 py-1 rounded-md text-sm hover:bg-[#C778DD] hover:text-black transition duration-300 ease-in-out"
+                      >
+                        {technology.name}
+                      </span>
+                    ))}
+                  </div>
                 </div>
+              </div>
             </div>
-        </>
-    )
-}
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ShowProject;
