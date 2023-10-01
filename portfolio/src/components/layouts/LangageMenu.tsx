b8d58c570fcf68097b44c697efae76899ee3f955
Alexandre Tressel <alexandre.tressel@epitech.eu>
2023-10-01 21:22:31 +0200

    WIP

--- portfolio/src/components/layouts/LangageMenu.tsx
+++ portfolio/src/components/layouts/LangageMenu.tsx
@@ -1,30 +1,51 @@
-import { useTranslation } from "react-i18next";
-import { LANGUAGES } from "../../constants"
-import { ChangeEvent, useState } from "react";
-
+import React, { useState } from 'react';
+import { IoLanguage } from 'react-icons/io5';
+import { FaFlag } from 'react-icons/fa';
 
 const LangageMenu = () => {
-    const { i18n } = useTranslation();
-    const [language, setLanguage] = useState<string>("fr")
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedLanguage, setSelectedLanguage] = useState('en'); // Default language
 
-    const changeLanguage = (lang: string) => {
-        setLanguage(lang)
-        i18n.changeLanguage(lang)
-    }
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const changeLanguage = (language: any) => {
+        setSelectedLanguage(language);
+        setIsOpen(false);
+        // You can add logic to change the application's language here
+    };
+
     return (
-        <>
-            <div className="dropdown dropdown-right dropdown-end fixed bottom-0 mb-16 ml-2 rounded-full">
-                <label tabIndex={0} className="btn m-1"><span className={`fi fi-${language} w-6 h-6 text-lg rounded-full border-0 shadow-none`}></span></label>
-                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    {LANGUAGES.map((lang) => (
-                        <li>
-                            <span className={`fi fi-${lang.code} w-7 h-7 rounded-full border-0 shadow-none`} onClick={() => changeLanguage(lang.code)}></span>
-                        </li>
-                    ))}
-                </ul>
+        <div className="fixed bottom-16 left-4">
+            <div className="relative">
+                <button
+                    onClick={toggleDropdown}
+                    className="bg-white rounded-full p-2 shadow-md focus:outline-none"
+                >
+                    <IoLanguage size={24} />
+                </button>
+                {isOpen && (
+                    <div className="absolute mt-2 p-2 rounded-full shadow-lg bg-white border -top-24 left-0">
+                        <ul className="flex flex-col space-y-2 rounded-lg">
+                            <button
+                                className="block text-sm text-start items-center hover:bg-gray-100"
+                                role="menuitem"
+                            >
+                                <span className='fi fi-gb text-lg' />
+                            </button>
+                            <button
+                                className="block text-sm text-start items-center hover:bg-gray-100"
+                                role="menuitem"
+                            >
+                                <span className='fi fi-gb text-lg' />
+                            </button>
+                        </ul>
+                    </div>
+                )}
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
-export default LangageMenu
+export default LangageMenu;
