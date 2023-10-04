0032e8937180e7f8c68c1d33aa42d6407537745a
Alexandre Tressel <alexandre.tressel@epitech.eu>
2023-10-04 15:15:11 +0200

    feat: :sparkles: Add download resume button and add tooltip on it

--- portfolio/src/components/layouts/DownloadResume.tsx
+++ portfolio/src/components/layouts/DownloadResume.tsx
@@ -1,15 +1,15 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { IoDownload } from "react-icons/io5";
 
 const DownloadResume = () => {
+  const { t } = useTranslation()
   const handleDownload = () => {
-    // Replace 'resume.pdf' with the actual path to your PDF file in the assets folder.
     const pdfFilePath = process.env.PUBLIC_URL + "/assets/cv.pdf";
 
-    // Create an anchor element and trigger a download.
     const a = document.createElement("a");
     a.href = pdfFilePath;
-    a.download = "resume.pdf"; // You can change the download filename if needed.
+    a.download = "resume.pdf";
     a.style.display = "none";
     document.body.appendChild(a);
     a.click();
@@ -18,8 +18,9 @@ const DownloadResume = () => {
 
   return (
     <div
-      className="bg-white rounded-full p-2 shadow-md focus:outline-none dark:hover:text-[#C778DD] hover:text-[#C778DD] transition duration-300 ease-in-out z-20"
+      className="bg-white rounded-full p-2 shadow-md focus:outline-none dark:hover:text-[#C778DD] hover:text-[#C778DD] transition duration-300 ease-in-out z-20 tooltip tooltip-right"
       onClick={handleDownload}
+      data-tip={t("actionbar.downloadResume")}
       style={{ cursor: "pointer" }}
     >
       <IoDownload size={24} />
