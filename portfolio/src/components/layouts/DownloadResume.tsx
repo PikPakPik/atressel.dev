598e1ee1b411d768cad492344f18f06fa44866bf
Alexandre.T <alexandre.tressel@icloud.com>
2023-10-08 18:34:46 +0200

    Finish download button

--- portfolio/src/components/layouts/DownloadResume.tsx
+++ portfolio/src/components/layouts/DownloadResume.tsx
@@ -3,25 +3,19 @@ import { useTranslation } from "react-i18next";
 import { IoDownload } from "react-icons/io5";
 
 const DownloadResume = () => {
-  const { t } = useTranslation()
-  const handleDownload = () => {
-    const pdfFilePath = process.env.PUBLIC_URL + "/assets/cv.pdf";
+  const { t } = useTranslation();
 
-    const a = document.createElement("a");
-    a.href = pdfFilePath;
-    a.download = "resume.pdf";
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  };
+  const downloadResume = () => {
+    const url = "cv.pdf";
+    window.open(url, "_blank");
+  }
 
   return (
     <div
       className="bg-white rounded-full p-2 shadow-md focus:outline-none dark:hover:text-[#C778DD] hover:text-[#C778DD] transition duration-300 ease-in-out z-20 tooltip tooltip-right"
-      onClick={handleDownload}
       data-tip={t("actionbar.downloadResume")}
       style={{ cursor: "pointer" }}
+      onClick={downloadResume}
     >
       <IoDownload size={24} />
     </div>
