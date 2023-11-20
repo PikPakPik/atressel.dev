e879392e9064f48dc52b44b9fbff0f41af936359
Alexandre.T <alexandre.tressel@icloud.com>
2023-11-20 22:13:06 +0100

    feat: :sparkles: Add better descriptions of projects

--- portfolio/src/data/projects.ts
+++ portfolio/src/data/projects.ts
@@ -6,7 +6,7 @@ import gitlabbackup from '../assets/img/projects/gitlabbackup.png';
 import discordbot from '../assets/img/projects/discordbot.png';
 import jobboard from '../assets/img/projects/jobboard.png';
 import epitale from '../assets/img/projects/epitale.png';
-import { BiLogoJava, BiLogoJavascript, BiLogoNodejs, BiLogoReact, BiLogoTailwindCss, BiLogoTypescript, BiSolidData, BiSolidObjectsHorizontalCenter } from "react-icons/bi";
+import { BiLogoJava, BiLogoJavascript, BiLogoNodejs, BiLogoReact, BiLogoTailwindCss, BiLogoTypescript, BiSolidData } from "react-icons/bi";
 import { SiCsharp, SiDiscord, SiDotnet, SiGitlab, SiGradle, SiNextdotjs, SiPrisma, SiScaleway, SiTypescript, SiUikit } from "react-icons/si";
 
 
@@ -14,7 +14,7 @@ export const projects: Project[] = [
     {
       id: 1,
       name: 'Portfolio',
-      description: 'Creation of my portfolio',
+      description: 'Création et publication de mon portfolio afin de me créer une identité numérique et permettre d\'avoir un support pour présenter mes projets.',
       image: portoflio,
       url: 'https://atressel.dev',
       technologies: [
@@ -53,7 +53,7 @@ export const projects: Project[] = [
     {
       id: 6,
       name: 'JobBoard',
-      description: 'Site de recherche d\'emploi',
+      description: 'Site de recherche d\'emploi avec des offres d\'emploi et des candidatures, ainsi qu\'un espace administrateur pour gérer les offres et les candidatures. Projet réalisé dans le cadre de ma formation à EPITECH.',
       image: jobboard,
       technologies: [
         {
@@ -78,7 +78,7 @@ export const projects: Project[] = [
     {
       id: 2,
       name: 'InfineoCData Client',
-      description: 'Envoie de requêtes SQL vers différents types de bases de données',
+      description: 'Client lourd permettant de se connecter à une base de données grâce à CData et d\'effectuer des requêtes SQL. Projet réalisé dans le cadre de mon stage de fin de BTS SIO.',
       image: infineocdataclient,
       technologies: [
         {
@@ -103,7 +103,7 @@ export const projects: Project[] = [
     {
       id: 3,
       name: 'Prisel.fr',
-      description: 'Site vitrine de l\'entreprise Prisel',
+      description: 'Site vitrine de l\'entreprise Prisel, évoluant dans le domaine du jeu vidéo.',
       image: prisel,
       url: 'https://prisel.fr',
       technologies: [
@@ -121,7 +121,7 @@ export const projects: Project[] = [
     {
       id: 4,
       name: 'BackupGitLab',
-      description: 'Sauvegarde des projets GitLab sur un bucket S3',
+      description: 'Sauvegarde des projets GitLab sur un bucket S3 de Scaleway. Projet réalisé dans le cadre de mon stage de fin de BTS SIO.',
       image: gitlabbackup,
       technologies: [
         {
@@ -146,7 +146,7 @@ export const projects: Project[] = [
     {
       id: 5,
       name: 'Bot Discord',
-      description: 'Bot Discord pour le serveur de l\'entreprise Prisel',
+      description: 'Bot Discord pour le serveur de l\'entreprise Prisel, permettant de gérer les rôles et les salons, ou encore de faire des sondages.',
       image: discordbot,
       technologies: [
         {
