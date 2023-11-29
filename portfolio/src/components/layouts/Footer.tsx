c0823ac5ae49901c7cd033a8321b8b7465e684de
Alexandre.T <alexandre.tressel@icloud.com>
2023-11-29 20:22:52 +0100

    fix: :bug: Correct size of footer text for mobile

--- portfolio/src/components/layouts/Footer.tsx
+++ portfolio/src/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const Footer: React.FC = () => {
     return (
         <footer className="text-gray-500 border-t-2 border-gray-500 h-20 items-center flex justify-center">
-            <div className="text-[2vh] sm:text-xs text-center ">
+            <div className="text-[1.5vh] text-center">
                 <span>@ Copyright 2023. Made with 💖 by Alexandre T.</span>
             </div>
         </footer>
