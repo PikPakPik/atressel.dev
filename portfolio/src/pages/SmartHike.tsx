940b21619000f6e4dc4007a1457192f3bcc7d478
Alexandre.T <alexandre.tressel@epitech.eu>
2024-08-26 10:01:09 +0200

    fix

--- portfolio/src/pages/SmartHike.tsx
+++ portfolio/src/pages/SmartHike.tsx
@@ -3,12 +3,9 @@ import store from '../assets/altstore.json';
 
 const SmartHike = () => {
   return (
-    <div>
-      <h1>Smart Hike</h1>
       <Routes>
-        <Route path="altstore_ipa" element={<pre>{JSON.stringify(store, null, 2)}</pre>} />
+        <Route path="altstore" element={<pre>{JSON.stringify(store, null, 2)}</pre>} />
       </Routes>
-    </div>
   );
 };
 
